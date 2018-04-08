9a6826416cc372dfdddde5af7ea4b59265e03154
Alexander Martynichev <septentrio2013@yandex.ru>
2018-04-08 20:33:23 +0400

    chart

--- src/main/java/ru/gitstats/repository/CommitRepository.java
+++ src/main/java/ru/gitstats/repository/CommitRepository.java
@@ -15,7 +15,19 @@ public interface CommitRepository extends JpaRepository<Commit, Long> {
     @Query(value = "select new ru.gitstats.business.model.CommitsModel(s.user.email, count(s)) from Commit s group by s.user.email ORDER BY count(s) DESC")
     List<CommitsModel> findAllCountGroupByEmail();
 
-    @Query(value = "SELECT  EMAIL,  d.count / (12 * (YEAR(d.max) - YEAR(d.min)) + (MONTH(d.max) - MONTH(d.min)))  as r FROM (SELECT EMAIL, min(DATE) AS min, max(DATE) AS max, count(*) as count  FROM commits GROUP BY EMAIL) AS d ORDER BY  r DESC", nativeQuery = true)
+    @Query(value = "SELECT\n" +
+            "  d.email,\n" +
+            "  d.count / (12 * (YEAR(d.max) - YEAR(d.min)) + (MONTH(d.max) - MONTH(d.min)))\n" +
+            "    AS r\n" +
+            "FROM\n" +
+            "  (SELECT\n" +
+            "     u.email,\n" +
+            "     min(DATE) AS min,\n" +
+            "     max(DATE) AS max,\n" +
+            "     count(*)  AS count\n" +
+            "   FROM users u LEFT JOIN commits c ON u.id = c.user_id\n" +
+            "   GROUP BY user_id) AS d\n" +
+            "ORDER BY r DESC", nativeQuery = true)
     List<Object[]> findAverageCountGroupByEmail();
 
 }
